174ed9a1ec5e0d1282f949596c1781aea51720eb
djampa kozlowski <dkozlowski@djampas-MacBook-Pro.local>
2023-10-25 17:40:45 +0200

    moved the main script outside the bin directory and deleted this directory

--- cONTent.py
+++ cONTent.py
@@ -1,20 +1,22 @@
 import sys
 import content.interface as intrf
+import numpy as np
 
 if __name__ == "__main__":
     args = intrf.arguments_parser()
     if args.sub_prog == "extract":
         import content.extraction as xtrct
-
         xtrct.main(args)
+        
     elif args.sub_prog == "distrib":
         import content.distribution as dstrb
 
         dstrb.main(args.input, args.outdir, args.fraction, args.prefix)
+        
     elif args.sub_prog == "coverage":
         import content.coverage as cvrg
-
         cvrg.main(args)
+        
     else:
         print(intrf.PROG_DESCRIPTION)
         sys.exit()
